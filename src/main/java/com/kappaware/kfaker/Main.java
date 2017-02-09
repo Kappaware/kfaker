280295dc885c44e744632bfe70a3ccc19732391c
Serge ALEXANDRE <sa@broadsoftware.com>
2017-02-09 15:58:16 +0100

    Initial commit

---
+++ src/main/java/com/kappaware/kfaker/Main.java
@@ -0,0 +1,17 @@
+package com.kappaware.kfaker;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class Main {
+	static Logger log = LoggerFactory.getLogger(Main.class);
+
+	
+	static public void main(String[] argv) {
+		log.info("kfaker start");
+		log.debug("kfaker DEBUG enabled");
+		
+	}
+
+}
